6c196463e914bdf3791bdd9916c36f6ebc56df7b
gbtwld <gbtwld@gmail.com>
2023-10-11 15:34:10 +0900

    crawler 수정

--- src/main.py
+++ src/main.py
@@ -9,15 +9,15 @@ Format_Date = Date.strftime('%Y/%m/%d %H시 %M분')
 
 lowest_price = getLowestPrice()
 
-# worksheet = GetSpreadSheet()
-# worksheet_length = len(worksheet.get_values())
-# prev_lowest_price = int(worksheet.acell(
-#     "B" + str(worksheet_length)).value.removeprefix("₩").replace(",", ""))
+worksheet = GetSpreadSheet()
+worksheet_length = len(worksheet.get_values())
+prev_lowest_price = int(worksheet.acell(
+    "B" + str(worksheet_length)).value.removeprefix("₩").replace(",", ""))
 
-# if (type(lowest_price) is int and lowest_price > 0):
-#     if (lowest_price < 600000):
-#         PostMessage(lowest_price)
-#     PostCurrentPrice(lowest_price, prev_lowest_price, Format_Date)
-#     UpdateSpreadSheet(worksheet_length, Format_Date, lowest_price)
-# else:
-#     print("Scrap Failed")
+if (type(lowest_price) is int and lowest_price > 0):
+    if (lowest_price < 600000):
+        PostMessage(lowest_price)
+    PostCurrentPrice(lowest_price, prev_lowest_price, Format_Date)
+    UpdateSpreadSheet(worksheet_length, Format_Date, lowest_price)
+else:
+    print("Scrap Failed")
