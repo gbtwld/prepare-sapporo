b0e17064b406c1e17619f28a900d42cbf3b10519
gbtwld <gbtwld@gmail.com>
2023-10-11 15:32:46 +0900

    crawler 수정

--- src/Crawler.py
+++ src/Crawler.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 URL = "https://flight.naver.com/flights/international/ICN-SPK-20240118/SPK-ICN-20240122?adult=5&isDirect=true&fareType=Y"
 
 options = webdriver.ChromeOptions()
-options.add_argument("--headless=chrome")
+# options.add_argument("--headless=chrome")
 options.add_argument('incognito')
 
 driver = webdriver.Chrome(options=options)
@@ -16,17 +16,16 @@ def getLowestPrice():
     lowest_price = 0
     try:
         driver.get(URL)
-
         driver.implicitly_wait(3)
-        WebDriverWait(driver, 30
-                      ).until_not(EC.presence_of_element_located((By.CLASS_NAME, "loadingProgress_loadingProgress__1LRJo")))
 
-        elements = driver.find_elements(By.CLASS_NAME, "item_num__3R0Vz")
-        for elem in elements:
-            elem_to_int = int(elem.get_attribute(
-                'innerText').replace(",", ""))
-            if (lowest_price == 0 or lowest_price > elem_to_int):
-                lowest_price = elem_to_int
+        WebDriverWait(driver, 30
+                      ).until_not(EC.presence_of_element_located((By.CLASS_NAME, "progressBar_progress__2VAEK")))
+
+        price_element = driver.find_element(By.CLASS_NAME, "item_num__3R0Vz")
+
+        lowest_price = int(price_element.get_attribute(
+            'innerText').replace(",", ""))
+
         return lowest_price
 
     except Exception as e:
