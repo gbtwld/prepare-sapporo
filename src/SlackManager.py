62c82b184aa60f94be8ecc24dec77464df034409
gbtwld <gbtwld@gmail.com>
2023-10-11 17:05:49 +0900

    슬랙 메시지 수정

--- src/SlackManager.py
+++ src/SlackManager.py
@@ -19,8 +19,7 @@ def PostCurrentPrice(curPrice: int, prevPrice: int, date: str):
     curPriceFormat = f"{curPrice:,}"
     rate = round((curPrice - prevPrice) / prevPrice * 100, 2)
     rate_string = str(rate) if rate < 0 else "+" + str(rate)
-    message = date + " 현재 항공권 가격은 " + \
-        curPriceFormat + "원 입니다. (" + rate_string + "%)"
+    message = "현재 항공권 가격은 " + curPriceFormat + "원 입니다. (" + rate_string + "%)"
     if (rate != 0):
         client.chat_postMessage(channel='#가격변동', text=message)
     else:
